88199c97e049cdfedc0a44d4e914a2f8747ac39b
Therese Komstadius <therese.komstadius@svt.se>
2017-08-02 13:33:38 +0200

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve('./src/index.js'),
+  output: {
+    path: path.resolve('./dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve('./src'),
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  devtool: 'cheap-module-eval-source-map'
+}
